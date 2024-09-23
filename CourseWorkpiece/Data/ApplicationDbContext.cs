1b21b2957ece4fa1ace3233c513be82fb66254e1
Phoen1X01 <yekaterina.krt@mail.ru>
2024-09-24 03:19:55 +0700

    2024-09-24 DB building connections

--- CourseWorkpiece/Data/ApplicationDbContext.cs
+++ CourseWorkpiece/Data/ApplicationDbContext.cs
@@ -21,10 +21,17 @@ namespace CourseWorkpiece.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<sGroup>()
-                .HasOne(u => u.User) //иметь 1 группу
-                .WithOne(g => g.sGroup) //с 1 юзером
-                .HasForeignKey<Group>(u => Users);
-                
+                .HasOne(g => g.User) //имеет 1 юзера
+                .WithOne(g => g.sGroup) //с 1 группой
+                .HasForeignKey<User>(u => u.sGroupId); //по ключу у юзера groupID
+
+            modelBuilder.Entity<Student>()
+                .HasOne(s => s.sGroup) //имеет 1 группу
+                .WithMany(g => g.Students) //с многими Students
+                .HasForeignKey(s => s.sGroupId);
+            //это связь 1 ко многим
+
+
 
         }
 
