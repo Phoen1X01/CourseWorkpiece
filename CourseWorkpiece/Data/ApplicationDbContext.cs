df751dc6b3d3d12773edf6d7330bd07cab55f834
Phoen1X01 <yekaterina.krt@mail.ru>
2024-09-25 19:38:40 +0700

    2024-09-25 DB Migration

--- CourseWorkpiece/Data/ApplicationDbContext.cs
+++ CourseWorkpiece/Data/ApplicationDbContext.cs
@@ -22,15 +22,24 @@ namespace CourseWorkpiece.Data
         {
             modelBuilder.Entity<sGroup>()
                 .HasOne(g => g.User) //имеет 1 юзера
-                .WithOne(g => g.sGroup) //с 1 группой
+                .WithOne(u => u.sGroup) //с 1 группой
                 .HasForeignKey<User>(u => u.sGroupId); //по ключу у юзера groupID
-
+            // 'nj cdzpm 1 r 1
             modelBuilder.Entity<Student>()
                 .HasOne(s => s.sGroup) //имеет 1 группу
                 .WithMany(g => g.Students) //с многими Students
-                .HasForeignKey(s => s.sGroupId);
+                .HasForeignKey(s => s.sGroupId); //ключ у студентов
             //это связь 1 ко многим
+            modelBuilder.Entity<Traffic>()
+                .HasOne(t => t.Student)
+                .WithMany(s => s.Traffics)
+                .HasForeignKey(t => t.StudentId);
 
+            modelBuilder.Entity<Lecture>()
+                .HasMany(l => l.Traffics)
+                .WithOne(t => t.Lecture)
+                .HasForeignKey(t => t.LectureId);
+                
 
 
         }
